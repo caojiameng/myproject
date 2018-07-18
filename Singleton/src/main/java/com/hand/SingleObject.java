8bcd77bb23a4de50ce9dc40fc05e934777a3d2d7
caojiameng <jiameng.cao@hand-china.com>
2018-07-18 16:18:07 +0800

    更新注释

--- Singleton/src/main/java/com/hand/SingleObject.java
+++ Singleton/src/main/java/com/hand/SingleObject.java
@@ -7,7 +7,15 @@ public class SingleObject {
     private SingleObject(){}
     //获取唯一可用的对象
     public static SingleObject getInstance(){
-        return instance;
+
+//        if (instance == null){
+//            instance = new SingleObject();  懒汉式，线程不安全
+//        }  线程安全需要在static后加synchorized
+
+
+
+
+        return instance;  //饿汉式
     }
     public void showMessage(){
         System.out.println("Hello World");
